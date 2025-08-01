8b41039b13bc092a4cd22a9ed9ab66b99a35c4a9
Lehlohonolo Mtshizana <lehlohonolomtshizana@icloud.com>
2025-08-01 15:11:43 +0200

    Consolidated templates in templates/relationship_app/, verified imports and data

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book, Library  
 
 def book_list(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})  
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'  
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
