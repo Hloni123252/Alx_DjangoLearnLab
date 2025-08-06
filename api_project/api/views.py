af41871a4538bfb89922994d78df59b9467aa50d
Lehlohonolo Mtshizana <lehlohonolomtshizana@icloud.com>
2025-08-06 13:00:14 +0200

    Completed CRUD operations with BookViewSet and Router in api_project

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,12 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.generics import ListAPIView
 
 class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
